07851a00e070d782270532d3d795d29bcb5e7c78
mathieso <kieran@dolfinity.com>
2013-10-29 15:40:12 -0400

    Spinner

--- photos1/server/choose_random_photo.php
+++ photos1/server/choose_random_photo.php
@@ -12,6 +12,9 @@ $number_photos = sizeof($image_data);
 //Choose a random index value.
 $photo_index = rand(0, $number_photos - 1);
 
+//Wait for a second. Simulates processing delay.
+sleep(1);
+
 //Return data about the random photo.
 echo json_encode( 
     array( 
